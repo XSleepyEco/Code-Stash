2e04cc0eaa23b0e93232bbdbafe461996637ba39
XSleepyEco <46331199+XSleepyEco@users.noreply.github.com>
2025-10-20 21:56:47 +0100

    End of day commit
    Did some work on calculation precedence

--- Python Code/Environment Test/main.py
+++ Python Code/Environment Test/main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+#Ex 3 Numbers and Math
+print("I will now count my chickens.") #String output
+
+print("Hens", 25 + 30 / 6) #Concats values with a space, the numeric section came out as a float, the division operator took precedence
+print("Roosters", 100 - 25 * 3 % 4) #Multiplcation takes precedence, then modulo, then the subtraction
+
+print("Now I will count the enggs:") #String output
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #Division took precedence, then modulo then the subtraction and addition
+
+print("Is it true that 3 + 2 < 5 - 7?") #String output
+print(3 + 2 < 5 - 7) #Logical output, additions and subrtraction are done first then the logical comparator
+
+print("What is 3 + 2?", 3 + 2) #Standard string and numeric output
+
+#Ex 4 Variables and names
