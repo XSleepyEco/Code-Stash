d85c224e2a05b3f0b2b0afed457bf3fdf595feaa
XSleepyEco <46331199+XSleepyEco@users.noreply.github.com>
2025-12-07 22:44:32 +0000

    Did up to Ex 32

---
+++ Python Code/Environment Test/Ex 28 Boolean Practice.py
@@ -0,0 +1,11 @@
+print(True and True) #True
+print(False and True) #False
+print(1 == 1 and 2 == 1) #False
+print("test" == "test") #True
+print(1 == 1 or 2 != 1) #True
+print(True and 1 == 1) #True
+print(False and 0 != 0) #False
+print(True or 1 == 1) #True
+print("test" == "testing") #False
+print(1 != 0 and 2 == 1) #False
+print("test" == 1) #False
