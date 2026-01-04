73a4ecf2bac4f137fde6ea11c3276448b6e5fd68
XSleepyEco <46331199+XSleepyEco@users.noreply.github.com>
2026-01-04 20:44:08 +0000

    Did ex 33-35

---
+++ Python Code/Environment Test/Ex 34 Accessing elements of a list.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+tiger = animals[1]
+penguin = animals[2]
+zebra = animals[3]
+
+print("The first animal is a bear: %s" % bear)
+print("The first animal is a tiger: %s" % tiger)
+print("The first animal is a penguin: %s" % penguin)
+print("The first animal is a zebra: %s" % zebra)
