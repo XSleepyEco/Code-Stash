9b6d9f4a39c97b464e80b37943d38b4ae83a748a
XSleepyEco <46331199+XSleepyEco@users.noreply.github.com>
2026-02-26 21:34:19 +0000

    Ex37-40

---
+++ Python Code/Environment Test/Ex 37 Symbol Review.py
@@ -0,0 +1,12 @@
+#print("Hello1 \\ boop") #escape character for \
+#print("Hello2 \' boop") #escape character for \
+#print("Hello3 \" boop") #escape character for \
+#print("Hello4 \a boop") #escape bell
+#print("Hello5 \b boop") #escape backspace
+#print("Hello6 \f boop") #escape formfeed
+#print("Hello7 \n boop") #escape linefeed
+print("Hello8 \r boop") #escape carraige return
+print("\r Hello8 boop") #escape carraige return
+print("Hello8 boop \r") #escape carraige return
+print("Hello9 \t boop") #escape tab
+print("Hello10 \v boop") #escapee vertical tab
